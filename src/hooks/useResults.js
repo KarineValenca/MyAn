a276efa8305f036b15f1284911c0b78b581b1d91
Karine Valenca <valenca.karine@gmail.com>
2020-12-26 18:40:09 -0300

    same app

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import jikan from '../api/jikan'
+
+export default() => {
+    const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const searchTopAnimeApi = async(page) => {
+        try{
+            const response = await jikan.get('/top/anime/1')
+            setResults(response.data.top)
+        } catch (e) {
+            setErrorMessage('Something went wrong, try again later')
+        }
+    }
+
+    const searchSeasonAnimeApi = async() => {
+        try {
+            const response = await jikan.get('/season/2020/winter')
+            setResults(response.data.anime)
+        } catch (e) {
+            setErrorMessage('Something went wrong, try again later')
+        }
+    }
+
+    useEffect(() => {
+        searchSeasonAnimeApi()
+    }, [])
+
+    return [searchSeasonAnimeApi, results, errorMessage]
+}
